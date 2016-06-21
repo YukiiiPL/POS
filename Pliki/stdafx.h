65ce9abbfebd31e77fe2d9f97f78e9d57a0641dd
YukiiiPL <golebiewski.t@gmail.com>
2016-06-21 19:09:18 +0200

    Plik z deklaracjami biblotek

---
+++ Pliki/stdafx.h
@@ -0,0 +1,32 @@
+// stdafx.h : include file for standard system include files,
+// or project specific include files that are used frequently, but
+// are changed infrequently
+//
+
+#pragma once
+
+#include "targetver.h"
+
+#include <stdio.h>
+#include <tchar.h>
+
+#include <iostream>
+#include <Windows.h>
+#include <vector>
+#include "string.h"
+#include "conio.h"
+#include "strsafe.h"
+
+#include "./INIParser.h"
+#include "./INIConfig.h"
+
+#include "opencv2/opencv.hpp"
+#include "opencv2/highgui/highgui.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
+
+using namespace cv;
+using namespace std;
+
+
+
+// TODO: reference additional headers your program requires here

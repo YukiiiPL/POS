0aeb1e72a3e117d6551ca6abb69a5d27ecf1a041
MicLew <milew93@gmail.com>
2016-06-23 18:39:39 +0200

    W pełni funkcjonalny program
    Zaimplementowane zostało wyrównywanie histogramu, obsługa pliku .INI oraz operacje odczytu i zapisu obrazów.

---
+++ PSC_Ychanger/stdafx.h
@@ -0,0 +1,35 @@
+// stdafx.h : include file for standard system include files,
+// or project specific include files that are used frequently, but
+// are changed infrequently
+//
+
+#pragma once
+
+#include "targetver.h"
+
+#include <stdio.h>
+#include <tchar.h>
+#include <atlstr.h>
+#include <sys/stat.h>
+#include <sys/stat.h>
+
+#include <iostream>
+#include <Windows.h>
+#include <vector>
+#include "string.h"
+#include "conio.h"
+#include "strsafe.h"
+
+#include "INIReader.h"
+#include "ini.h"
+
+#include "opencv2/opencv.hpp"
+#include "opencv2/highgui/highgui.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
+
+using namespace cv;
+using namespace std;
+
+
+
+// TODO: reference additional headers your program requires here
